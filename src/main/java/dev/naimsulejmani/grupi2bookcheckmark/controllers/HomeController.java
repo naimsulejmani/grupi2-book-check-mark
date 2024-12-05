49124c33b2abf104eaec4918f9bd8e0d5e4bc831
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-12-05 11:46:19 +0100

    Started Cookie for Java

--- src/main/java/dev/naimsulejmani/grupi2bookcheckmark/controllers/HomeController.java
+++ src/main/java/dev/naimsulejmani/grupi2bookcheckmark/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package dev.naimsulejmani.grupi2bookcheckmark.controllers;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.UUID;
 
 @Controller
 public class HomeController {
 
     @GetMapping("")
-    public String home() {
+    public String home(HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("id")) {
+                    System.out.println("Cookie found: " + cookie.getValue());
+                    return "redirect:/authors";
+                }
+            }
+        }
+
+        Cookie cookie = new Cookie("id", UUID.randomUUID().toString());
+        cookie.setMaxAge(60 * 60 * 24 * 365); // 1 year
+        response.addCookie(cookie);
         return "index";
     }
 }
