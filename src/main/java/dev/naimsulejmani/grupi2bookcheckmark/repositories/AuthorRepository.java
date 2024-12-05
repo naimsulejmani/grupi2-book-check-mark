caa073e72ae5d4e77eda806775387c84d550b2d0
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-12-05 10:45:38 +0100

    author repository method added

--- src/main/java/dev/naimsulejmani/grupi2bookcheckmark/repositories/AuthorRepository.java
+++ src/main/java/dev/naimsulejmani/grupi2bookcheckmark/repositories/AuthorRepository.java
@@ -4,10 +4,13 @@ import dev.naimsulejmani.grupi2bookcheckmark.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     Optional<Author> findByEmail(String email); // SELECT * FROM authors WHERE email = ?;
     Long countAllByEmailContaining(String email);// SELECT COUNT(*) FROM authors WHERE email LIKE %?%;
+    List<Author> findAllByNameContainingOrSurnameContainingOrderByBirthdateDesc(String name, String surname);
+    // SELECT * FROM authors WHERE name LIKE %?% OR surname LIKE %?%;
 }
